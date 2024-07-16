2bc88bc207e920f74ecbbb9dc7462ff8b676c409
AbdoMossad <smsm22online@gmail.com>
2024-07-16 13:31:01 +0300

    Update template.js

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
 document.getElementById('submit-template').addEventListener('click', function() {
     const jsonInput = document.getElementById('json-input').value;
     localStorage.setItem('jsonTemplate', jsonInput);
-    alert('Template saved!');
+    //alert('Template saved!');
     // Redirect to the editor page
     window.location.href = 'editor.html';
 });
